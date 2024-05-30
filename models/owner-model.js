81659e3aeebc09d44d3954ca7b03d377d7f8002b
naina0x0 <mm2101888@gmail.com>
2024-05-30 21:41:05 +0530

    seperation of concern

---
+++ models/owner-model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+
+
+
+const ownerSchema= mongoose.Schema({
+    fullname: {
+        type:String,
+        minLength:3,
+        trim:true
+    },
+    email: String,
+    password: String,
+    
+   
+    products: {
+        typeof:Array,
+        default: []
+    },
+    contact: Number,
+    picture: String,
+    gstin:Number
+})
+
+module.exports=mongoose.model("owner", ownerSchema);
---
+++ models/user-model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+
+
+
+const userSchema= mongoose.Schema({
+    fullname: {
+        type:String,
+        minLength:3,
+        trim:true
+    },
+    email: String,
+    password: String,
+    cart: {
+        typeof:Array,
+        default: []
+    },
+    isadmin: Boolean,
+    orders: {
+        typeof:Array,
+        default: []
+    },
+    contact: Number,
+    picture: String
+})
+
+module.exports=mongoose.model("user", userSchema);
