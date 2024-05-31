656513b52daec6cd825df1ee5b0ade3eae1ba690
Manish <mm2101888@gmail.com>
2024-05-31 13:58:09 +0530

    31st May

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
 
 const db = require("./config/mongoose-connection");
 
@@ -12,7 +15,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.get("/", function(req,res, next){
-res.send("hey");
-});
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productsRouter);
+
 app.listen(3000);
